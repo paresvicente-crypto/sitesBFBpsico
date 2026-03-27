80f0764f12a7e9a26347de850a9a974156346961
paresvicente-crypto <paresvicente@gmail.com>
2026-03-27 18:23:23 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,13 @@
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
-import Benefits from "@/components/Benefits";
+import Benefits from "../components/Benefits";
 import Projects from "@/components/Projects";
 import Process from "@/components/Process";
 import Differentials from "@/components/Differentials";
 import Testimonials from "@/components/Testimonials";
 import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 
 const Index = () => (
   <>
